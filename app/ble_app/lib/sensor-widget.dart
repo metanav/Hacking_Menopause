503088ef2dadb1a258fc214413afb4ece9c8221b
Naveen Kumar <>
2020-03-21 16:35:13 +0900

    Add StreamBuilder for sensor readings

--- app/ble_app/lib/sensor-widget.dart
+++ app/ble_app/lib/sensor-widget.dart
@@ -1,74 +1,70 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
-import 'package:weather_icons/weather_icons.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'string-extension.dart';
 
-class SensorState extends State<Sensor> {
-  List<int> valueBytes;
+class Sensor extends StatelessWidget {
+  final String label;
+  final BluetoothCharacteristic readCharacteristic;
+  final icon = {
+    'temperature': FaIcon(FontAwesomeIcons.thermometer, size: 100),
+    'accelerometer': FaIcon(FontAwesomeIcons.running, size: 100)
+  };
 
-  void initState() {
-    super.initState();
-    valueBytes = [];
-    widget.readCharacteristic.value.listen((value) {
-      setState(() {
-        valueBytes = value;
-      });
-    });
-  }
+  Sensor({Key key, @required this.label, @required this.readCharacteristic})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    String readings;
-    var icon;
-    var text = Text('');
-
-    if (valueBytes.length > 0) {
-      if (widget.label == 'temperature') {
-        double tempC =
-            ((((valueBytes[1] << 8) | (valueBytes[0] & 0xff)).toSigned(16) / 64.0) / 4.0) + 25.0;
-        print(valueBytes);
-        print(tempC);
-        readings = '$tempC\u00B0 C';
-        icon = BoxedIcon(WeatherIcons.thermometer,
-            color: Colors.pinkAccent, size: 96.0);
-        text = Text('$readings', style: TextStyle(fontSize: 48.0));
-      } else if (widget.label == 'accelerometer') {
-
-        double accelX =
-            (((valueBytes[1] << 8) | (valueBytes[0] & 0xff)).toSigned(16) / 16.0);
-        double accelY =
-            (((valueBytes[3] << 8) | (valueBytes[2] & 0xff)).toSigned(16) / 16.0);
-        double accelZ =
-            (((valueBytes[5] << 8) | (valueBytes[4] & 0xff)).toSigned(16) / 16.0);
-
-        readings = 'X: $accelX Y: $accelY Z: $accelZ';
-        icon = Icon(Icons.directions_run, color: Colors.pinkAccent, size: 96.0);
-        text = Text('$readings', style: TextStyle(fontSize: 24.0));
-      }
-    }
-
     return Scaffold(
         appBar: AppBar(
-          title: Text(widget.label.toString().capitalize()),
+          title: Text(label.toString().capitalize()),
         ),
         body: Container(
             child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Center(child: icon), 
-                  Center(child: text.data.length > 0 ? text : CircularProgressIndicator())
-                ])));
+              Center(child: icon[label]),
+              StreamBuilder(
+                  stream: readCharacteristic.value,
+                  builder: (BuildContext context, snapshot) {
+                    if (snapshot.hasData && snapshot.data.length > 0) {
+                      return Center(child: _buildSensorWidget(snapshot.data));
+                    }
+                    return CircularProgressIndicator();
+                  }
+              )
+            ]
+          )
+        )
+      );
   }
-}
 
-class Sensor extends StatefulWidget {
-  final String label;
-  final BluetoothCharacteristic readCharacteristic;
+  Widget _buildSensorWidget(data) {
+    Widget widget;
 
-  Sensor({Key key, @required this.label, @required this.readCharacteristic})
-      : super(key: key);
+    switch (label) {
+      case 'temperature':
+        double tempC =
+            ((((data[1] << 8) | (data[0] & 0xff)).toSigned(16) / 64.0) / 4.0) +
+                25.0;
 
-  @override
-  SensorState createState() => SensorState();
+        widget = Text('$tempC\u00B0 C', style: TextStyle(fontSize: 48.0));
+        break;
+      case 'accelerometer':
+        double accelX =
+            (((data[1] << 8) | (data[0] & 0xff)).toSigned(16) / 16.0);
+        double accelY =
+            (((data[3] << 8) | (data[2] & 0xff)).toSigned(16) / 16.0);
+        double accelZ =
+            (((data[5] << 8) | (data[4] & 0xff)).toSigned(16) / 16.0);
+
+        widget = Text('X: $accelX Y: $accelY Z: $accelZ',
+            style: TextStyle(fontSize: 24.0));
+        break;
+    }
+
+    return widget;
+  }
 }
